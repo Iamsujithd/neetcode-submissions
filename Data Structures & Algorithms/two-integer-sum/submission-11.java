49773fc70a730f73a3baf2d82004b5f398862d01
Iamsujithd <146457405+Iamsujithd@users.noreply.github.com>
2026-05-24 17:55:11 +0530

    Add: two-integer-sum - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum/submission-11.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> numToIndex =new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            int curr=nums[i];
+            int comp=target-curr;
+            if(numToIndex.containsKey(comp)){
+                return new int[] {numToIndex.get(comp),i};
+            }
+            numToIndex.put(curr,i);
+
+        }
+        throw new IllegalArgumentException("Error arg1");
+    }
+};
