1bbd8481738873af354c575eaf4fd8bcaaec866b
Iamsujithd <146457405+Iamsujithd@users.noreply.github.com>
2026-07-02 20:02:38 +0530

    Add: length-of-last-word - submission-0

---
+++ Data Structures & Algorithms/length-of-last-word/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        if(s.length()==1)return 1;
+        int i=s.length()-1,j;
+        while(i>=0 && s.charAt(i)==' '){
+            i--;
+        }
+        j=i;
+        while(i>=0 && s.charAt(i)!=' '){
+            i--;
+        }
+        return j-i;
+    }
+}
